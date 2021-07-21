17de4ba08a5190c4d5e7913c3760faf27876ecb7
yf <1919138974@qq.com>
2021-07-21 23:56:14 +0800

    补充

--- demo.js
+++ demo.js
@@ -1,24 +1,39 @@
-// 模拟 bind
-Function.prototype.bind1 = function() {
-  // 将参数拆解为数组
-  const args = Array.prototype.slice.call(arguments) // 变成数组
+class Solution {
+  public:
+      void backtrack(vector<vector<int>>& res, vector<int>& output, int first, int len){
+          // 所有数都填完了
+          if (first == len) {
+              res.emplace_back(output);
+              return;
+          }
+          for (int i = first; i < len; ++i) {
+              // 动态维护数组
+              swap(output[i], output[first]);
+              // 继续递归填下一个数
+              backtrack(res, output, first + 1, len);
+              // 撤销操作
+              swap(output[i], output[first]);
+          }
+      }
+      vector<vector<int>> permute(vector<int>& nums) {
+          vector<vector<int> > res;
+          backtrack(res, nums, 0, (int)nums.size());
+          return res;
+      }
+  };
   
-  // 获取 this（数组第一项）
-  const t = args.shift()
-  
-  // fn1.bind(...) 中的 fn1
-  const self = this
-  
-  // 返回一个函数
-  return function() {
-      return self.apply(t, args)
-  }
-}
-function fn1(a, b, c){
-  console.log('this', this)
-  console.log(a, b, c)
-  return 'this is fn1'
-}
-const fn2 = fn1.bind1({x: 100}, 10, 20, 30)
-const res = fn2()
-console.log(res) 
+// let a1 = [1,2]
+// let a2 = [3,4]
+// function swap(a1,a2){
+//   let t = a1
+//   a1 = a2
+//   a2 = t
+// }
+// function swap1(a1){
+//   let t = a1[0]
+//   a1[0] = a1[1]
+//   a1[1] = t
+// }
+// // swap(a1,a2)
+// swap1(a1)
+// console.log(a1,a2);
