23962a9d4dc7145cf7ad8cf02c19fc6653b7ca23
HanxingYf <1919138974@qq.com>
2021-08-19 19:14:45 +0800

    a

---
+++ 剑指offer/103.js
@@ -0,0 +1,9 @@
+var coinChange = function(coins, amount) {
+    let cnt = 0
+    
+};
+
+// dp[i] 总金额为 i 所需最少金币数为 dp[i]
+
+
+coins = [1, 2, 5], amount = 11
---
+++ 面试题/8.11.js
@@ -0,0 +1,24 @@
+// var waysToChange = function(n) {
+
+// };
+
+// // 1, 5, 10, 25
+// // 20
+// // dp[i] i 分有dp[i] 中方法 
+// //  dp[i] = dp[i-1] 
+
+
+let a = 'c'
+
+// const f = () =>{
+//     console.log(a);
+// }
+
+const b = {
+    run: () =>{
+        console.log(a);
+    }
+}
+
+b.run()
+
